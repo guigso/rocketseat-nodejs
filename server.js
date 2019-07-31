e90b8d1f009855bfbab7a7982a561c2478dd0ec8
Guigso <gui.soliveira@hotmail.com>
2019-07-31 16:42:38 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+//Iniciando app
+const app = express();
+
+//Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
+requireDir('./src/models');
+
+
+//Primeira rota
+app.use('/api', require('./src/routes'));
+
+app.listen(3001);
